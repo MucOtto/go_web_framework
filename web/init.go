78f7b4c8af6b418aaa36940356b0c7a46eed8db6
root <1730495747@qq.com>
2024-05-14 13:36:07 +0800

    分组路由实现

--- web/init.go
+++ web/init.go
@@ -2,13 +2,28 @@ package web
 
 import "net/http"
 
-type handlerFunc func(w http.ResponseWriter, r *http.Request)
-
-type router struct {
+type routerGroup struct {
+	name           string
 	handlerFuncMap map[string]handlerFunc
 }
 
-func (r *router) Add(name string, handlerFunc handlerFunc) {
+func (r *router) Group(name string) *routerGroup {
+	routerGroup := &routerGroup{
+		name:           name,
+		handlerFuncMap: make(map[string]handlerFunc),
+	}
+
+	r.routerGroups = append(r.routerGroups, routerGroup)
+	return routerGroup
+}
+
+type handlerFunc func(w http.ResponseWriter, r *http.Request)
+
+type router struct {
+	routerGroups []*routerGroup
+}
+
+func (r *routerGroup) Add(name string, handlerFunc handlerFunc) {
 	r.handlerFuncMap[name] = handlerFunc
 }
 
@@ -19,15 +34,17 @@ type Engine struct {
 func New() *Engine {
 	return &Engine{
 		router: router{
-			handlerFuncMap: make(map[string]handlerFunc),
+			routerGroups: make([]*routerGroup, 0),
 		},
 	}
 }
 
 func (e *Engine) Run() {
 	// 路由和功能的映射
-	for key, value := range e.handlerFuncMap {
-		http.HandleFunc(key, value)
+	for _, group := range e.routerGroups {
+		for key, value := range group.handlerFuncMap {
+			http.HandleFunc("/"+group.name+key, value)
+		}
 	}
 
 	err := http.ListenAndServe(":8080", nil)
