5be5d40336db49dc3909b9224773fbcdfd4987c4
root <1730495747@qq.com>
2024-05-22 12:30:57 +0800

    用户自定义日志输出级别

--- web/log/log.go
+++ web/log/log.go
@@ -7,6 +7,24 @@ import (
 	"time"
 )
 
+const (
+	greenBg   = "\033[97;42m"
+	whiteBg   = "\033[90;47m"
+	yellowBg  = "\033[90;43m"
+	redBg     = "\033[97;41m"
+	blueBg    = "\033[97;44m"
+	magentaBg = "\033[97;45m"
+	cyanBg    = "\033[97;46m"
+	green     = "\033[32m"
+	white     = "\033[37m"
+	yellow    = "\033[33m"
+	red       = "\033[31m"
+	blue      = "\033[34m"
+	magenta   = "\033[35m"
+	cyan      = "\033[36m"
+	reset     = "\033[0m"
+)
+
 type LoggerLevel int
 
 type LoggerFormatter struct {
@@ -57,16 +75,55 @@ func (l *Logger) Print(level LoggerLevel, msg any) {
 	l.Formatter.Level = level
 	formatter := l.Formatter.formatter(msg)
 	for _, out := range l.Outs {
+		if out == os.Stdout {
+			l.Formatter.Color = true
+			formatter = l.Formatter.formatter(msg)
+		}
 		fmt.Fprint(out, formatter)
 	}
 }
 
 func (f *LoggerFormatter) formatter(msg any) string {
 	now := time.Now()
-	return fmt.Sprintf("[otto] %v | level=%s | msg=%#v \n",
+	if f.Color {
+		//要带颜色  error的颜色 为红色 info为绿色 debug为蓝色
+		levelColor := f.LevelColor()
+		msgColor := f.MsgColor()
+		return fmt.Sprintf("%s [otto] %s %s%v%s | level= %s %s %s | msg=%s %#v %s \n",
+			yellow, reset, blue, now.Format("2006/01/02 - 15:04:05"), reset,
+			levelColor, f.Level.Level(), reset, msgColor, msg, reset,
+		)
+	}
+	return fmt.Sprintf("[otto] %v | level=%s | msg= %#v \n",
 		now.Format("2006/01/02 - 15:04:05"),
 		f.Level.Level(), msg,
 	)
+}
+
+func (f *LoggerFormatter) LevelColor() string {
+	switch f.Level {
+	case LevelDebug:
+		return blue
+	case LevelInfo:
+		return green
+	case LevelError:
+		return red
+	default:
+		return cyan
+	}
+}
+
+func (f *LoggerFormatter) MsgColor() string {
+	switch f.Level {
+	case LevelDebug:
+		return ""
+	case LevelInfo:
+		return ""
+	case LevelError:
+		return red
+	default:
+		return cyan
+	}
 }
 
 func (level LoggerLevel) Level() string {
